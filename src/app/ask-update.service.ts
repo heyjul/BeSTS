aab89e41d061ea622e87985bc652b554f6db0d16
Julien Heyligers <julien.heyligers@igroove.be>
2022-11-01 10:23:46 +0100

    Ask user to update the app

---
+++ src/app/ask-update.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SwUpdate } from '@angular/service-worker';
+import { ConfirmDialogComponent } from './shared/components/confirm-dialog/confirm-dialog.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AskUpdateService {
+  constructor(private swUpdate: SwUpdate,
+    private dialog: MatDialog) {
+    swUpdate.versionUpdates.subscribe(_ => this.askUserToUpdate());
+  }
+
+  askUserToUpdate() {
+    console.log('Asking for update');
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '80%',
+      data: { message: 'Une nouvelle version est disponible, voulez-vous la télécharger ?' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result)
+        window.location.reload();
+    });
+  }
+}
